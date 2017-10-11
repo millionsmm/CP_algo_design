9fc94d071b1717b715b4dc82194a28abfa9715df
millionsmm <linweipeng96@qq.com>
2017-10-11 21:37:43 +0800

    美团点评笔试题

---
+++ algorithm/src/main/java/com/example/NumberConstruct.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by Wilber
+ * on 11/10/2017.
+ * 对于一个十进制数而言，它的数位和等于将它各位数字相加得到的和。比如 231 的数位和是 6，3179 的数位和是 20。
+ * 现在你知道某个十进制数的数位和等于 s，并且这个数不包含 0，且任意相邻的数位是不同的，
+ * 比如 112 或者 102 都是不满足条件的。现在你想知道满足这样的条件的最大的数是多少？
+ */
+
+public class NumberConstruct {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.print(getCount(num));
+    }
+
+    public static String getCount(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (num > 0) {
+            if (num % 3 == 0) {
+                stringBuilder.append(1);
+                num -= 1;
+            }
+            if (num % 3 == 2) {
+                stringBuilder.append(2);
+                num -= 2;
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
---
+++ algorithm/src/main/java/com/example/xuliecaouzo.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by Wilber
+ * on 11/10/2017.
+ * 有一天你得到了一个长度为 n 的序列，序列中的元素分别是 1,2,3,…,n。接下来你想对这个序列进行一些操作。
+ * 每一次操作你会选择一个数然后将它从序列中原来的位置取出并放在序列的最前面。
+ * 你想知道经过一系列操作后这个序列长什么样。
+ *
+ * @ 输入
+ * 第一行包含两个整数n,m，分别表示序列的长度和操作的个数。1 ≤ n,m≤ 105
+ * 接下来m行每行包含一个整数ki，表示你要把ki放到序列的最前面。1 ≤ ki ≤ n
+ * @ 输出
+ * 从前往后输出序列中的每个元素，每个元素占一行。
+ */
+//超出时间限制
+public class xuliecaouzo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int count = in.nextInt();
+        String string = "";
+        for (int i = 1; i <= n; i++) {
+            string += i;
+        }
+        int num[] = new int[count];
+        for (int i = 0; i < count; i++) {
+            num[i] = in.nextInt();
+        }
+
+
+        for (int i = 0; i < count; i++) {
+            String[] temp = string.split(String.valueOf(num[i]));
+            string = String.valueOf(num[i]);
+//            for (int j = 0; j < temp.length; j++) {
+//                string += temp[j];
+//            }
+            for (String s : temp) {
+                string += s;
+            }
+        }
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(string.charAt(i));
+        }
+
+    }
+}
