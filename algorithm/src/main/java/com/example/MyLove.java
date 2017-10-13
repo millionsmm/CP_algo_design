42b6b99c41bb77fca7897dbc1022efd20ab2c495
millionsmm <linweipeng96@qq.com>
2017-10-13 22:35:46 +0800

    mylove

---
+++ algorithm/src/main/java/com/example/MyLove.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Created by Wilber
+ * on 2017/10/13.
+ * 输入N，M，两人轮流报数R，1<=R<=M,令N=N-R,谁先使N为0或者负数谁赢，
+ * 假如你是其中一人，求第一次报什么数必赢。
+ * 输出所有可能，如果没有，则输出1
+ */
+
+public class MyLove {
+    public static void main(String[] args){
+
+    }
+}
