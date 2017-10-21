341880f571bf1f7b26b7aeb0999f8db980a496e7
millionsmm <linweipeng96@qq.com>
2017-10-21 18:56:53 +0800

    使用栈求和为某数字的所有情况

---
+++ algorithm/src/main/java/com/example/SumOfTarget.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import java.util.Stack;
+
+/**
+ * Created by Wilber
+ * on 21/10/2017.
+ * 给出数组n和目标数字s
+ * 输出求所有和为s的组合
+ */
+
+public class SumOfTarget {
+    private static final int TARGET_SUM = 15;
+    private static final int[] DATA = {1, 3, 4, 5, 6, 7, 2, 8, 9, 10, 11, 15, 13, 14};
+    private static Stack<Integer> stack = new Stack<>();
+    private static int sumInStack = 0;
+
+    public static void main(String args[]) {
+        populateSubset(DATA, 0, DATA.length);
+    }
+
+    /**
+     * 使用栈的方法
+     *
+     * @param data
+     * @param fromIndex
+     * @param endIndex
+     */
+    private static void populateSubset(final int[] data, int fromIndex, int endIndex) {
+        if (sumInStack >= TARGET_SUM) {
+            if (sumInStack == TARGET_SUM) {
+                print(stack);
+            }
+            return;
+        }
+        for (int currentIndex = fromIndex; currentIndex < endIndex; currentIndex++) {
+            if (sumInStack + data[currentIndex] <= TARGET_SUM) {
+                stack.push(data[currentIndex]);
+                sumInStack += data[currentIndex];
+                populateSubset(data, currentIndex + 1, endIndex);
+                sumInStack -= stack.pop();
+            }
+        }
+    }
+
+    private static void print(Stack<Integer> stack) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(TARGET_SUM).append("=");
+        for (Integer integer : stack) {
+            sb.append(integer).append("+");
+        }
+        System.out.println(sb.deleteCharAt(sb.length() - 1).toString());
+    }
+}
+
