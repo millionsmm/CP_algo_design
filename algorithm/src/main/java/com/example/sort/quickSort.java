48028dc32d96a1220f4bcbd5106b8eff4399a176
millionsmm <linweipeng96@qq.com>
2017-10-22 23:01:22 +0800

    普通快排

---
+++ algorithm/src/main/java/com/example/sort/quickSort.java
@@ -0,0 +1,37 @@
+package com.example.sort;
+
+/**
+ * Created by Wilber
+ * on 22/10/2017.
+ */
+
+public class quickSort {
+    /**
+     * 普通快排
+     *
+     * @param a
+     * @param p
+     * @param r
+     */
+    public static void quickSort(int a[], int p, int r) {
+        if (p < r) {
+            int q = partition(a, p, r);
+            quickSort(a, p, q - 1);
+            quickSort(a, q + 1, r);
+        }
+    }
+
+    public static int partition(int a[], int p, int r) {
+        int i = p;
+        int j = r;
+        int x = a[p];
+        while (i < j) {
+            while (i < j && a[j] > x) j--;
+            if (i < j) a[i] = a[j];
+            while (i < j && a[i] < x) i++;
+            if (i < j) a[j] = a[i];
+        }
+        a[i] = x;
+        return i;
+    }
+}
