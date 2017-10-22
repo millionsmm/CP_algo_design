706f5581a9574e46e3fe236ed73dda95cc8c8652
millionsmm <linweipeng96@qq.com>
2017-10-22 11:36:25 +0800

    动态申请权限

--- app/src/main/java/millionsmm/designmodereview/MainActivity.java
+++ app/src/main/java/millionsmm/designmodereview/MainActivity.java
@@ -1,13 +1,40 @@
 package millionsmm.designmodereview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //没有权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE);
+        } else {
+            //有权限的逻辑
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //用户同意授权
+
+                } else {
+                    //用户拒绝授权
+
+                }
+                break;
+        }
     }
 }
