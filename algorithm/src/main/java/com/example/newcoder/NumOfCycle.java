87a5c690ceaae2751bdfeb83f9bc276dee926be3
millionsmm <linweipeng96@qq.com>
2017-11-08 22:47:31 +0800

    - cycle

--- algorithm/src/main/java/com/example/newcoder/NumOfCycle.java
+++ algorithm/src/main/java/com/example/newcoder/NumOfCycle.java
@@ -1,5 +1,6 @@
 package com.example.newcoder;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -7,23 +8,32 @@ import java.util.Set;
 /**
  * Created by Wilber
  * on 8/11/2017.
+ * 求循环单词的种树
  */
 
 public class NumOfCycle {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        in.nextLine();
         String words[] = new String[n];
         for (int i = 0; i < n; i++) {
             words[i] = in.nextLine();
         }
-        System.out.println();
+        System.out.println(getNum(words));
     }
 
     private static int getNum(String words[]) {
         int result = 0;
-
-
+        HashMap<String, Set<String>> map = new HashMap<>();
+        for (String word : words) {
+            Set<String> set = getStrings(word);
+            if (!map.containsValue(set)) {
+                result++;
+            } else {
+                map.put(word, set);
+            }
+        }
         return result;
     }
 
