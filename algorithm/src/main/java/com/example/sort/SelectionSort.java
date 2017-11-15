6d7cb8f00c5e6a54995b5b5aa5e40e23b4217d69
millionsmm <linweipeng96@qq.com>
2017-11-15 18:11:10 +0800

    - selection sort

---
+++ algorithm/src/main/java/com/example/sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package com.example.sort;
+
+/**
+ * Created by Wilber
+ * on 2017/11/15.
+ */
+
+public class SelectionSort {
+    public void selectionSort(int a[]) {
+        if (a.length < 2 || a == null) return;
+        for (int i = 0; i < a.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                minIndex = a[j] < a[minIndex] ? j : minIndex;
+            }
+            swap(a, i, minIndex);
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+}
