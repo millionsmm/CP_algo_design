a1c415057d4cb615c174ca848507f7fd1afc24dd
millionsmm <2415122956@qq.com>
2017-11-24 18:31:17 +0800

    - get number of k

---
+++ algorithm/src/main/java/com/example/SwordRefersToOffer/GetNumberOfK.java
@@ -0,0 +1,19 @@
+package com.example.SwordRefersToOffer;
+
+/**
+ * Created by Wilber
+ * on 2017-11-24 .
+ * 统计一个数字在排序数组中出现的次数
+ */
+
+public class GetNumberOfK {
+    public int getNumberOfK(int[] array, int k) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == k) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
