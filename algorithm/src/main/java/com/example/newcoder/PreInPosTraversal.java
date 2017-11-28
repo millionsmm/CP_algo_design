fb919b07faaf0411142f7e1847d21f0c36559d52
millionsmm <2415122956@qq.com>
2017-11-28 18:43:02 +0800

    - pos un Recur

--- algorithm/src/main/java/com/example/newcoder/PreInPosTraversal.java
+++ algorithm/src/main/java/com/example/newcoder/PreInPosTraversal.java
@@ -66,6 +66,27 @@ public class PreInPosTraversal {
         }
     }
 
+    public void posOrderUnRecur(TreeNode root) {
+        if (root != null) {
+            Stack<TreeNode> s1 = new Stack<>();
+            Stack<TreeNode> s2 = new Stack<>();
+            s1.push(root);
+            while (!s1.isEmpty()) {
+                root = s1.pop();
+                s2.push(root);
+                if (root.left != null) {
+                    s1.push(root.left);
+                }
+                if (root.right != null) {
+                    s1.push(root.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.print(s2.pop().value + " ");
+            }
+        }
+    }
+
     public class TreeNode {
         public int value;
         public TreeNode left;
