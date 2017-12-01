30346b757cef8af977f477c31256c496f0c61bc9
millionsmm <2415122956@qq.com>
2017-12-01 18:35:58 +0800

    - maxPoints

---
+++ algorithm/src/main/java/com/example/leetcode/MaxPoints.java
@@ -0,0 +1,25 @@
+package com.example.leetcode;
+
+/**
+ * Created by Wilber
+ * on 2017-12-01 .
+ * Given n points on a 2D plane,
+ * find the maximum number of points that lie on the same straight line.
+ */
+
+public class MaxPoints {
+    class Point {
+        int x;
+        int y;
+
+        Point() {
+            x = 0;
+            y = 0;
+        }
+
+        Point(int a, int b) {
+            x = a;
+            y = b;
+        }
+    }
+}
