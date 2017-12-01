097fe032d9710f1507cde2104b206808734824a7
millionsmm <2415122956@qq.com>
2017-12-01 18:32:36 +0800

    - evalRPN

---
+++ algorithm/src/main/java/com/example/leetcode/evalRPN.java
@@ -0,0 +1,45 @@
+package com.example.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by Wilber
+ * on 2017-12-01 .
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are+,-,*,/. Each operand may be an integer or another expression.
+ * Some examples:
+ * ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ */
+
+public class evalRPN {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            try {
+                int num = Integer.parseInt(tokens[i]);
+                stack.add(num);
+            } catch (Exception e) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.add(get(a, b, tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+
+    private int get(int a, int b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+}
