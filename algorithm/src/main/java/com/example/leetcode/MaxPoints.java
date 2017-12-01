a501c6cf24fd6aba44aea1981578e8c56ffb104d
millionsmm <2415122956@qq.com>
2017-12-01 19:50:59 +0800

    - maxPoints

--- algorithm/src/main/java/com/example/leetcode/MaxPoints.java
+++ algorithm/src/main/java/com/example/leetcode/MaxPoints.java
@@ -8,6 +8,42 @@ package com.example.leetcode;
  */
 
 public class MaxPoints {
+    public int maxPoints(Point[] points) {
+        int length = points.length;
+        if (length < 2) return length;
+        int max = 2;
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i].x;
+            int y = points[i].y;
+            int num = 1;
+            int temp = 0;
+            for (int j = i + 1; j < points.length; j++) {
+                int gapx1 = x - points[j].x;
+                int gapy1 = y - points[j].y;
+                if (gapx1 == 0 && gapy1 == 0) {
+                    temp++;
+                } else {
+                    num++;
+                    for (int k = j + 1; k < points.length; k++) {
+                        int gapx2 = x - points[k].x;
+                        int gapy2 = y - points[k].y;
+                        if (gapx1 * gapy2 == gapy1 * gapx2) {
+                            num++;
+                        }
+                    }
+                    if (max < num + temp) {
+                        max = num + temp;
+                    }
+                }
+                num = 1;
+            }
+            if (max < num + temp) {
+                max = num + temp;
+            }
+        }
+        return max;
+    }
+
     class Point {
         int x;
         int y;
